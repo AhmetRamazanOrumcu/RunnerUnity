98292f56663cb971fa7a9ec58f7ee0c7c419103b
Ahmet Ramazan Örümcü <ahmetramazanmr@outlook.com>
2025-08-30 22:43:36 +0300

    Initial commit

---
+++ Assets/_project/Scripts/Elements/Player.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float speed;
+    public Transform cameraHolder;
+    public float sensitivity;
+    public float maxWith;
+
+    private float _initalXPos;
+    private float _horizontalInput;
+    private void Update()
+    {
+        print(transform.position);
+        //transform.position = new Vector3(10, 10, 10);
+
+        //transform.position +=Vector3.forward * Time.deltaTime*speed;
+
+        
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            _initalXPos = transform.position.x;
+            //print(_initalXPos);
+
+        }
+        if(Input.GetMouseButton(0))
+        {
+            _horizontalInput += Input.GetAxis("Mouse X")/Screen.width;
+            var pos = transform.position;
+            pos.x = _initalXPos + _horizontalInput*sensitivity;
+            transform.position = pos;
+
+            CalmPlayerPosition();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _horizontalInput = 0;
+        }
+
+        
+        transform.position += Vector3.forward * Time.deltaTime * speed;
+
+        var cameraPos = transform.position;
+        cameraPos.x = 0;
+        cameraHolder.position = cameraPos;
+    }
+
+    private void CalmPlayerPosition()
+    {//sýnýrlarý belirlemek için hareketin
+        var pos = transform.position;
+        pos.x=Mathf.Clamp(pos.x,-maxWith,maxWith);
+        transform.position = pos;
+    }
+}
